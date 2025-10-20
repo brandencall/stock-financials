cd7719a2927fdfa18b5fe84ab4712aad5a2fbc06
Branden Call <brandencall@live.com>
2025-10-20 12:43:50 -0600

    Update to database path

--- data-processor/src/main.cpp
+++ data-processor/src/main.cpp
@@ -2,7 +2,7 @@
 #include "utils.h"
 
 int main() {
-    std::filesystem::path dbPath = getDataDirectory();
+    std::filesystem::path dbPath = getDataDirectory() / "stock_data.db";
     Application app(dbPath);
     app.run();
     return 0;
