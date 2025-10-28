89b9a49c840bfd3cd93061ea773fa8b4862a4d05
Branden Call <brandencall@live.com>
2025-10-28 09:57:56 -0600

    Changed the data path again

--- api/src/main.cpp
+++ api/src/main.cpp
@@ -3,9 +3,9 @@
 #include <httplib.h>
 
 int main() {
-    // std::filesystem::path dataDir= "../data/stock_data.db";
-    std::filesystem::path dataDir = "/var/lib/stockfinances";
-    db::Database db(dataDir);
+    // std::filesystem::path dataFile= "../data/stock_data.db";
+    std::filesystem::path dataFile= "/var/lib/stockfinances/stock_data.db";
+    db::Database db(dataFile);
     Server server(db);
     server.registerControllers();
     int port = 8080;
