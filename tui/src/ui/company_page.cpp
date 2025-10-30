2aa6806ec85dc0412db6e5f50ca64d5375fd011d
Branden Call <brandencall@live.com>
2025-10-30 13:57:54 -0600

    sorting financial records desc. Displaying revenue example

--- tui/src/ui/company_page.cpp
+++ tui/src/ui/company_page.cpp
@@ -1,13 +1,19 @@
 #include "ui/company_page.h"
 #include "application.h"
 #include "models.h"
+#include <functional>
+#include <iomanip>
 #include <ncurses.h>
+#include <sstream>
 
 CompanyPage::CompanyPage(Application &app, const Company &company) : app(app), company(company), refreshNeeded(false) {
     clear();
     printw("Fetching data...");
     refresh();
-    companyData = app.getApiClient().getCompaniesAnnualFinancials(company.cik, 1);
+    companyData = app.getApiClient().getCompaniesAnnualFinancials(company.cik, 5);
+    std::sort(
+        companyData.reports.begin(), companyData.reports.end(),
+        [](const FinancialReport &a, const FinancialReport &b) { return a.filing.filedDate > b.filing.filedDate; });
 }
 
 void CompanyPage::render() {
@@ -15,12 +21,19 @@ void CompanyPage::render() {
     int line = 2;
     for (int i = 0; i < companyData.reports.size(); ++i) {
         FinancialReport report = companyData.reports[i];
-        mvprintw(line, 2, "%s - %s", report.filing.form.c_str(), report.filing.fp.c_str());
+        mvprintw(line, 2, "%s - %s - %s", report.filing.form.c_str(), std::to_string(report.filing.fy).c_str(),
+                 report.filing.fp.c_str());
         line++;
         for (int j = 0; j < report.facts.size(); ++j) {
             FinancialFact fact = report.facts[j];
-            mvprintw(line, 2, "%s : %s", fact.tag.c_str(), std::to_string(fact.value).c_str());
-            line++;
+            if (fact.tag == "Revenue") {
+                double revenue = fact.value;
+                std::ostringstream oss;
+                oss.imbue(std::locale(""));
+                oss << std::fixed << std::setprecision(2) << revenue;
+                mvprintw(line, 2, "Revenue: %s", oss.str().c_str());
+                line++;
+            }
         }
     }
 }
