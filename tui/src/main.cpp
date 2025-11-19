3983e16d9ce260784f8223d2a23be2a26d88487c
Branden Call <brandencall@live.com>
2025-11-19 11:54:24 -0700

    tui formatting

--- tui/src/main.cpp
+++ tui/src/main.cpp
@@ -1,15 +1,14 @@
 #include "api_client.h"
 #include "application.h"
-#include <ncurses.h>
 #include <locale.h>
+#include <ncurses.h>
 
 int main() {
     setlocale(LC_ALL, "");
-    
+
     ApiClient client;
     Application app(client);
     app.initUi();
     app.run();
     return 0;
 }
-
